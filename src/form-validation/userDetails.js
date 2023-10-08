d57bb91418d42d46629a0dc5489b352790566627
ojingwaDaniel <ojingwadanny@gmail.com>
2023-10-08 23:35:56 +0100

    added a line of code

--- src/form-validation/userDetails.js
+++ src/form-validation/userDetails.js
@@ -1,15 +1,17 @@
-// import { useState } from "react"
+import { useState } from "react"
 
 export default function UsersLogin(){
-    // const[email,setEmail] = useState('')
-    // const[password,setPassword]= useState('')
-    // const [confirmPassowrd, setConfirmPassword] = useState("");
-    return(
-        <div className="form">
-                <input type="email" name="" id="" />
-                <input type="password" name="" id="" />
-                <input type="password" name="" id="" />
-                <input type="submit" value="" />
-        </div>
-    )
-}
+    const[email,setEmail] = useState('')
+    const[password,setPassword]= useState('')
+    const [confirmPassowrd, setConfirmPassword] = useState('');
+    const [message,setMessage] = useState('')
+    function FindErrors(){
+        const errors = []
+        if(!email|| !password || confirmPassowrd) errors.push('each items need to be filled in')
+        if([...email].filter(i=> i === '@').length!==1) errors.push(' there should be one exact @ sign')
+        if(password === confirmPassowrd) errors.push('passwords must match')
+        setMessage(errors.length ? errors.join(', ') : 'user Created')
+        return errors
+    }
+   
+} 
