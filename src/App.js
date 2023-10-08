ce375e622b34950bc9312bbe4c7d3295ae8e548b
ojingwaDaniel <ojingwadanny@gmail.com>
2023-10-08 22:57:02 +0100

    added a line of code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 // import ColorRender from './colorRender/colorRender';
-import DarkMode from './dark-mode/dark-mode';
+// import DarkMode from './dark-mode/dark-mode';
+import UsersLogin from './form-validation/userDetails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <ColorRender/> */}
-        <DarkMode/>
+        {/* <DarkMode/> */}
+        <UsersLogin/>
+
 
 
        
---
+++ src/form-validation/userDetails.js
@@ -0,0 +1,15 @@
+// import { useState } from "react"
+
+export default function UsersLogin(){
+    // const[email,setEmail] = useState('')
+    // const[password,setPassword]= useState('')
+    // const [confirmPassowrd, setConfirmPassword] = useState("");
+    return(
+        <div className="form">
+                <input type="email" name="" id="" />
+                <input type="password" name="" id="" />
+                <input type="password" name="" id="" />
+                <input type="submit" value="" />
+        </div>
+    )
+}
