c98f241e6f93663f8070f029534bd879dca66997
ojingwaDaniel <ojingwadanny@gmail.com>
2023-10-08 23:45:19 +0100

    added a line of code

--- src/form-validation/userDetails.js
+++ src/form-validation/userDetails.js
@@ -10,8 +10,21 @@ export default function UsersLogin(){
         if(!email|| !password || confirmPassowrd) errors.push('each items need to be filled in')
         if([...email].filter(i=> i === '@').length!==1) errors.push(' there should be one exact @ sign')
         if(password === confirmPassowrd) errors.push('passwords must match')
-        setMessage(errors.length ? errors.join(', ') : 'user Created')
         return errors
     }
-   
+    const handleSubmit = e=>{
+        e.preventDefault()
+        const errors = FindErrors()
+        setMessage(errors.length ? errors.join(", ") : "user Created");
+        
+
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" onChange={e=>setEmail(e.target.value)}/>
+            <label htmlFor="password">Password</label>
+            
+        </form>
+    )
 } 
