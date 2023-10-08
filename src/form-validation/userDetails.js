e7f78c94dc4fb93055e12a80b585216fb495d01a
ojingwaDaniel <ojingwadanny@gmail.com>
2023-10-09 00:13:31 +0100

    completed the form validation

--- src/form-validation/userDetails.js
+++ src/form-validation/userDetails.js
@@ -7,9 +7,9 @@ export default function UsersLogin(){
     const [message,setMessage] = useState('')
     function FindErrors(){
         const errors = []
-        if(!email|| !password || confirmPassowrd) errors.push('each items need to be filled in')
+        if(!email|| !password || !confirmPassowrd) errors.push('each items need to be filled in')
         if([...email].filter(i=> i === '@').length!==1) errors.push(' there should be one exact @ sign')
-        if(password === confirmPassowrd) errors.push('passwords must match')
+        if(password !== confirmPassowrd) errors.push('passwords must match')
         return errors
     }
     const handleSubmit = e=>{
@@ -19,12 +19,29 @@ export default function UsersLogin(){
         
 
     }
-    return(
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" onChange={e=>setEmail(e.target.value)}/>
-            <label htmlFor="password">Password</label>
-            
-        </form>
-    )
+    return (
+      <form onSubmit={handleSubmit}>
+        <h2>Sign up!</h2>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          name="confirm password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {message}
+        <input type="submit" value="Submit" />
+      </form>
+    );
 } 
