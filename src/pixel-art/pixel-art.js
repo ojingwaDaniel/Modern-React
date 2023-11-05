a7184586b95f00053fe353f8ab3b2cf35d79fe6e
ojingwaDaniel <ojingwadanny@gmail.com>
2023-11-05 20:52:56 +0100

    repeated the code for mastering

--- src/pixel-art/pixel-art.js
+++ src/pixel-art/pixel-art.js
@@ -1,64 +1,64 @@
 import { useContext, useState } from "react";
 import { createContext } from "react";
 
-// const ColorContest = createContext({
-//     color : 'lightGray',
-//     setColor : ()=> {}
-// })
-// const ColorPicker = ()=>{
-//     const {setColor} = useContext(ColorContest)
-//     const colors = [
-//       "red",
-//       "blue",
-//       "yellow",
-//       "green",
-//       "black",
-//       "white",
-//       "purple",
-//     ];
-//     return(
-//         <div>
-//             <h1>Pick a color</h1>
-//             {colors.map(color=> (
-//                 <button type="button" key={color} style={{backgroundColor : color}} onClick={()=> setColor(color)}></button>
-//             ))}
-//         </div>
+const ColorContest = createContext({
+    color : 'lightGray',
+    setColor : ()=> {}
+})
+const ColorPicker = ()=>{
+    const {setColor} = useContext(ColorContest)
+    const colors = [
+      "red",
+      "blue",
+      "yellow",
+      "green",
+      "black",
+      "white",
+      "purple",
+    ];
+    return(
+        <div>
+            <h1>Pick a color</h1>
+            {colors.map(color=> (
+                <button type="button" key={color} style={{backgroundColor : color}} onClick={()=> setColor(color)}></button>
+            ))}
+        </div>
 
-//     )
+    )
 
-// }
-// const Pixel = ()=>{
-//     const {color} = useContext(ColorContest)
-//     const [pixelColor, setPixelColor] = useState('lightgrey')
-//     return (
-//       <button
-//         type="button"
-//         onClick={() => setPixelColor(color)}
-//         style={{
-//           height: "20px",
-//           width: "20px",
-//           backgroundColor: pixelColor,
-//           margin: "1px",
-//         }}
-//       ></button>
-//     );
-// }
-// const Pixels = ()=>{
-//    const  pixels = []
-//     for (let index = 0; index <1000 ; index++) pixels.push(<Pixel key={index}/>)
-//     return (
-//       <div
-//         style={{
-//           display: "grid",
-//           gridTemplateColumns: "repeat(10, 1fr)",
-//           width: "210px",
-//           margin: "0 auto",
-//         }}
-//       >
-//         {pixels}
-//       </div>
-//     );
-// }
+}
+const Pixel = ()=>{
+    const {color} = useContext(ColorContest)
+    const [pixelColor, setPixelColor] = useState('lightgrey')
+    return (
+      <button
+        type="button"
+        onClick={() => setPixelColor(color)}
+        style={{
+          height: "20px",
+          width: "20px",
+          backgroundColor: pixelColor,
+          margin: "1px",
+        }}
+      ></button>
+    );
+}
+const Pixels = ()=>{
+   const  pixels = []
+    for (let index = 0; index <1000 ; index++) pixels.push(<Pixel key={index}/>)
+    return (
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(10, 1fr)",
+          width: "210px",
+          margin: "0 auto",
+        }}
+      >
+        {pixels}
+      </div>
+    );
+}
 // export default function PixelArt(){
 //     const [color,setColor] = useState('lightGray')
 //     return(
@@ -69,6 +69,9 @@ import { createContext } from "react";
 //         </ColorContest.Provider>
 //     )
 // }
+
+
+// repeated code for mastering
 const ColorContext = createContext({
   color : 'lightgrey',
   setColor :()=> {}
